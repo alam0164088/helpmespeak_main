73ada63595e1e1f018a0471304b9c6b7bf68bd44
alam0164088 <alam01640882530@gmail.com>
2025-11-08 05:21:32 +0600

    update

--- myproject/settings.py
+++ myproject/settings.py
@@ -75,15 +75,14 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "django.middleware.sessions.middleware.SessionMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",   # ← আগে
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware", # ← পরে
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
 ]
-
 # ------------------------------
 # URLs & WSGI
 # ------------------------------
