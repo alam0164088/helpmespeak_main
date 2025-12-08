51f2e5fc75295084b1369fc895bf3958a926b77d
alam0164088 <alam01640882530@gmail.com>
2025-12-09 02:01:07 +0600

    update

--- authentication/signals.py
+++ authentication/signals.py
@@ -1,8 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 from .models import User, Profile
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
+        # নতুন ইউজারের জন্য profile create
         Profile.objects.create(user=instance)
+    else:
+        # existing ইউজারের profile check করে update
+        try:
+            profile = instance.profile
+        except ObjectDoesNotExist:
+            Profile.objects.create(user=instance)
+        else:
+            profile.save()
--- myproject/settings.py
+++ myproject/settings.py
@@ -168,12 +168,18 @@ MEDIA_ROOT = BASE_DIR / "media"
 # ------------------------------
 # Email
 # ------------------------------
-EMAIL_BACKEND = env("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
-EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = env.int("EMAIL_PORT", default=587)
-EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
-EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
+EMAIL_BACKEND = env("EMAIL_BACKEND")
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env.int("EMAIL_PORT")
+EMAIL_USE_SSL = env.bool("EMAIL_USE_SSL")
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+
+
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="no-reply@helpmespeak.app")
+
+
 
 # ------------------------------
 # CORS
